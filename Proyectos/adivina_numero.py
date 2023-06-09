ff00cb8539744c4b662acb2376c60480d97e45c8
Hastur5 <mgasca12@gmail.com>
2023-06-08 23:05:10 -0600

    feat: se añade ejercicio de llenado de palabras y el inicio de número aleatorio.

---
+++ Proyectos/adivina_numero.py
@@ -0,0 +1,9 @@
+import random 
+
+def adivina_el_numero(x):
+    print("=====================")
+    print("¡Bienvenido al juego!")
+    print("=====================")
+    print("Tu meta es adivinar el número generado por la computadora")
+
+    numero_aleatorio = random.randint(1,x)
---
+++ Proyectos/historias_locas.py
@@ -0,0 +1,14 @@
+# organizacion = "Mau"
+
+# print("Aprende a programar con " + organizacion)
+# print("Aprende a programar con {}".format(organizacion))
+# print(f"Aprende a programar con {organizacion}")
+
+adj = input("Adjetivo: ")
+verbo1 = input("Verbo: ")
+verbo2 =input("Verbo: ")
+sustantivo_plural = input("Sustantivo (plural): ")
+
+madlib = f"¡Progrmar es tan {adj}! Siempre me emociona porque me encanta {verbo1} problemas. ¡Aprende a {verbo2} con Mau y alcanza tus {sustantivo_plural}!"
+
+print(madlib)
