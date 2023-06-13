1b579f4ae7227b00e2bef82ccb22b8effe6deb48
Hastur5 <mgasca12@gmail.com>
2023-06-12 19:29:57 -0600

    feat: se añade juego de adivinación

--- Proyectos/adivina_numero.py
+++ Proyectos/adivina_numero.py
@@ -1,9 +1,25 @@
 import random 
 
+
 def adivina_el_numero(x):
     print("=====================")
     print("¡Bienvenido al juego!")
     print("=====================")
     print("Tu meta es adivinar el número generado por la computadora")
 
-    numero_aleatorio = random.randint(1,x)
+    numero_aleatorio = random.randint(1, x)
+
+    prediccion = 0
+
+    while prediccion != numero_aleatorio:
+        prediccion = int(input(f"Adivina un número entre 1 y {x}: "))
+
+        if prediccion < numero_aleatorio:
+            print("Intenta nuevamente. Este número es muy chiquito, como tú.")
+        elif prediccion > numero_aleatorio:
+            print("Intenta otra vez. Este número es muy grande.")
+
+    print(f"¡Felicitaciones! Adivinaste el número {numero_aleatorio} correctamente.")
+
+
+adivina_el_numero(10)
